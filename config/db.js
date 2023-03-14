42c4fa0ffdd8fdf19a227d72ed0da0ec35e56953
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-03-14 19:38:30 +0100

    Ajout de nouveau paramètre de config temporaire

--- config/db.js
+++ config/db.js
@@ -2,3 +2,10 @@ exports.db_host= "localhost"
 exports.db_username = "root"
 exports.db_password = ""
 exports.db_name = "isis"
+
+exports.option = option = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'isis'
+}
