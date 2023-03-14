39e08ead1c79f19199e18b5ee8ffd86a85629e8d
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-03-14 21:48:57 +0100

    Système plugin
    Création du système de plugin + ajout de couleur via un plugin

--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -1,61 +1,63 @@
-const express = require('express')
-const router = express.Router()
-const AdminControllers = require('../Controllers/AdminControllers')
-const securite = require('../system/RoleCheking.js')
+const express = require("express");
+const router = express.Router();
+const AdminControllers = require("../Controllers/AdminControllers");
+const securite = require("../system/RoleCheking.js");
+const fs = require("fs");
+var colors = require('colors/safe');
 
+const multer = require("multer");
+const env = require("../var");
+const config = require("../config.json");
 
+const upload = multer({ dest: env.dirname + "/public/upload/" });
 
+router.get("/", securite, AdminControllers.admin);
 
+router.get("/user", securite, AdminControllers.user);
+//Gestion images
+router.get("/pics", securite, AdminControllers.pics);
 
+router.post(
+  "/pics.send",
+  securite,
+  upload.single("pics"),
+  AdminControllers.pics_send
+);
+router.get("/pics.delete", securite, AdminControllers.pics_delete);
 
-const multer = require('multer')
-const env = require('../var')
-const config = require('../config.json')
+//Gestion des documents
 
+router.get("/docs", securite, AdminControllers.docs);
 
-const upload = multer({ dest: env.dirname + '/public/upload/' })
+router.post(
+  "/file.send",
+  securite,
+  upload.single("file"),
+  AdminControllers.docs_send
+);
+router.get("/file.delete", securite, AdminControllers.docs_delete);
 
+//Gestion redirection
 
-
-router.get('/', securite, AdminControllers.admin )
-
-
-router.get('/user', securite , AdminControllers.user )
-//Gestion images 
-router.get('/pics', securite , AdminControllers.pics )
-
-router.post('/pics.send', securite,upload.single('pics'), AdminControllers.pics_send)
-router.get('/pics.delete', securite, AdminControllers.pics_delete)
-
-//Gestion des documents 
-
-router.get('/docs', securite, AdminControllers.docs)
-
-router.post('/file.send', securite, upload.single('file'), AdminControllers.docs_send)
-router.get('/file.delete', securite, AdminControllers.docs_delete)
-
-
-//Gestion redirection 
-
-router.get('/redirect', securite, AdminControllers.redirect)
-router.post('/redirect/send', securite, AdminControllers.redirect_send)
-router.get('/redirect/remove', securite, AdminControllers.redirect_remove)
+router.get("/redirect", securite, AdminControllers.redirect);
+router.post("/redirect/send", securite, AdminControllers.redirect_send);
+router.get("/redirect/remove", securite, AdminControllers.redirect_remove);
 
 //Upload TinyMCE
 
-router.post('/upload',securite, upload.single('file'), (req, res) => {
-    
-    // Traitez l'image et renvoyez la réponse à TinyMCE
-    const imageUrl = '/public/upload/' + req.file.filename;
-    res.json({ location: imageUrl });
+router.post("/upload", securite, upload.single("file"), (req, res) => {
+  // Traitez l'image et renvoyez la réponse à TinyMCE
+  const imageUrl = "/public/upload/" + req.file.filename;
+  res.json({ location: imageUrl });
 });
-//Plugin 
+//Plugin
 
+const pluginsPath = "./Plugins";
+fs.readdirSync(pluginsPath).forEach((file) => {
+  const plugin = require(`${env.dirname}/Plugins/${file}/Admin/`);
 
+  router.use(plugin.route, plugin.router);
+  console.log( colors.green(" [ok] Admin of " + plugin.info.name + " loaded"));
+});
 
-
-
-
-
-
-module.exports = router
+module.exports = router;
--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const app = express();
 const config = require('./config.json');
 const db_config = require('./config/db')
 const env = require('./var')
+const fs = require('fs')
+var colors = require('colors/safe');
 
-console.log('ISIS CMS STARTING');
+console.log( colors.blue.underline( 'ISIS CMS STARTING \n')  );
 const bodyParser = require('body-parser'); 
 const session = require('express-session')
 app.use(bodyParser.text({ type: 'text/html' }))
@@ -44,6 +46,24 @@ app.use('/user', User_Routes)
 app.use('/admin', Admin_Routes)
 app.use('/asset', express.static(__dirname + '/Admin/asset'))
 app.use('/Themes',express.static(__dirname + '/Themes'))
+
+
+//Plugin loading 
+
+const pluginsPath = './Plugins';
+fs.readdirSync(pluginsPath).forEach(file => {
+
+  const plugin = require(`${pluginsPath}/${file}`);
+  colors.enable()
+
+  app.use(plugin.route, plugin.router);
+  console.log( colors.green(' [ok] Plugin '+ plugin.info.name +  ' loaded,  version : '+ plugin.info.version +  ' \n'))
+  
+});
+
+
+
+
 app.use('/', Main_Routes)
 
 
@@ -51,5 +71,5 @@ app.use('/', Main_Routes)
 
 
 app.listen(config.app_port, () => {
-    console.log("Serveur listening on http://localhost:"+config.app_port)
+    console.log(colors.blue.underline("\nServeur listening on http://localhost:"+config.app_port + '\n'))
 })
