8d584131e5e0e9c7d28fddadb0d2828f7f0e2ded
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-04-13 11:55:22 +0200

    Simplification du code

--- Controllers/UserControllers.js
+++ Controllers/UserControllers.js
@@ -1,95 +1,85 @@
-const db = require('../System/db')
-const bcrypt = require('bcrypt')
-const session = require('../config/session')
+const db = require("../System/db");
+const connection = require('../System/db')
+const bcrypt = require("bcrypt");
+const session = require("../config/session");
 
+exports.connect = async (req, res) => {
+  if (!req.body.email) {
+    return;
+  }
 
-
-exports.connect = (req, res ) => {
-
-    if (req.body.email) {
-        let SQL = `SELECT * FROM user WHERE email="${req.body.email}" `
-        db.query(SQL, (err, result) => {
-            if (err) {
-                console.log('NEW DATABASE ERROR USERCONTROLLER: ' + err)
-
-            }
-            if (result) {
-                const formi = JSON.stringify(result)
-                const reformed = JSON.parse(formi)
-                const formed = reformed[0]
-              
-               
-                bcrypt.compare(req.body.password, formed.password, (err, isMatch) => {
-                    if (err) {
-                        console.log('USER CONTROLLER HASH ERROR: ' + err )
-                        
-                    }
-                    if (isMatch) {
-                        req.session.regenerate((err) => {
-                            // Gérez les erreurs si nécessaire
-                            if (err) {
-                              console.error(err);
-                              return res.status(500).send('Erreur de serveur');
-                            }
-                            req.session.userID = formed.id
-                        req.session.username = formed.username
-                        req.session.email = formed.email
-                        req.session.role = formed.role
-                        req.session.isLoged = true
-                       
-                        res.redirect('/profil')
-                        
-                        })
-                        
-                        
-                        
-
-                        
-                    }
-                })
-            }
-        })
-
-    }
-
-    if (req.body.username) {
-
-    }
+  try {
     
+    const SQL = `SELECT * FROM user WHERE email = "${req.body.email}"`;
+    const result = await db.awaitQuery(SQL);
+   
+    if (!result || result.length === 0) {
+      res.redirect("/user/login");
+      return;
+    }
+
+    const [user] = result;
+
+    if (!user.password) {
+      res.redirect("/user/login");
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    if (!isMatch) {
+      res.redirect("/user/login");
+      return;
+    }
+
+    req.session.regenerate((err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Erreur de serveur");
+      }
+
+      req.session.userID = user.id;
+      req.session.username = user.username;
+      req.session.email = user.email;
+      req.session.role = user.role;
+      req.session.isLoged = true;
+
+      res.redirect("/profil");
+    });
+  } catch (error) {
+    console.log("NEW ERROR USERCONTROLLER: " + error + error.stack);
+    res.redirect("/user/login");
+  }
+};
 
 
 
-}
 
-exports.create = (req, res ) => {
-
-    bcrypt.hash(req.body.password, session.saltRounds, (err, hash) => {
-        if (err) {}
-       let data = {
-        email: req.body.email,
-        username: req.body.username,
-        password: hash
-       }
-       console.log(data)
-       let SQL = `SELECT * FROM user WHERE email="${data.email}"`
-       db.query(SQL, (err, result) => {
-        let formi = JSON.stringify(result)
-        let formed = JSON.parse(formi)
-        if (data.email === formed.email) {
-            
-        } else {
-            db.query('INSERT INTO user SET ? ', data , (error, resultat) => {
-                if (error) {
-                    console.log('NEW DATABASE ERROR : ' + error)
-                }
-                if (resultat) {
-                    res.redirect('/user/login')
-                }
-            })
-        }
-       })
-    })
-
-
-
-}
+exports.create = (req, res) => {
+  bcrypt.hash(req.body.password, session.saltRounds, (err, hash) => {
+    if (err) {
+    }
+    let data = {
+      email: req.body.email,
+      username: req.body.username,
+      password: hash,
+    };
+    console.log(data);
+    let SQL = `SELECT * FROM user WHERE email="${data.email}"`;
+    db.query(SQL, (err, result) => {
+      let formi = JSON.stringify(result);
+      let formed = JSON.parse(formi);
+      if (data.email === formed.email) {
+      } else {
+        db.query("INSERT INTO user SET ? ", data, (error, resultat) => {
+          if (error) {
+            console.log("NEW DATABASE ERROR : " + error);
+          }
+          if (resultat) {
+            res.redirect("/user/login");
+          }
+        });
+      }
+    });
+  });
+};
