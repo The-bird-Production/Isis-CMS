a6206d317d08c434213ad26f0ab7532767dea9d3
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-04-13 11:55:08 +0200

    Ajout de la possibilité d'await une query

--- System/db.js
+++ System/db.js
@@ -1,17 +1,33 @@
 const mysql = require('mysql')
+const amysql = require('mysql-await')
 const dbconfig = require('../config/db')
 
 
-module.exports = db = mysql.createPool({
+module.exports = db = mysql.createConnection({
     host: dbconfig.db_host,
     user: dbconfig.db_username,
     password: dbconfig.db_password,
     database: dbconfig.db_name 
 })
 
-db.getConnection( (err, connection)=> {
-    if (err) {
-        console.log('NEW DATABASE ERROR : ' + err)
+
+db.connect((error) => {
+    if (error) {
+      console.error('Error connecting to database: ' + error.stack);
+      return;
     }
-    console.log ("DB connected successful: " + connection.threadId)
+    console.log('Connected to database with threadId: ' + db.threadId);
+  });
+
+  module.exports = connection = amysql.createConnection({
+    host: dbconfig.db_host,
+    user: dbconfig.db_username,
+    password: dbconfig.db_password,
+    database: dbconfig.db_name 
 })
+connection.on(`error`, (err) => {
+    console.error(`Connection error ${err.code}`);
+  });
+  
+
+  
