813d481fa5b362751cc41494205f83754a4a7f59
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-04-16 00:05:45 +0200

    Add file for server update

--- update/modele.sql
+++ update/modele.sql
@@ -1,65 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : sam. 15 avr. 2023 à 21:57
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données : `isis`
+--
 
-DROP TABLE IF EXISTS file;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
 CREATE TABLE IF NOT EXISTS `file` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  file varchar(100) NOT NULL,
-  url varchar(100) NOT NULL,
-  PRIMARY KEY (id)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS img;
-CREATE TABLE IF NOT EXISTS img (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  title varchar(100) NOT NULL,
-  alt varchar(100) NOT NULL,
-  path varchar(100) NOT NULL,
-  date date NOT NULL,
-  PRIMARY KEY (id)
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `img`
+--
+
+DROP TABLE IF EXISTS `img`;
+CREATE TABLE IF NOT EXISTS `img` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `alt` varchar(100) NOT NULL,
+  `path` varchar(100) NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS page;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
 CREATE TABLE IF NOT EXISTS `page` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  title varchar(100) NOT NULL,
-  url varchar(100) NOT NULL,
-  body text NOT NULL,
-  type varchar(100) DEFAULT NULL,
-  PRIMARY KEY (id)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `body` text NOT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS redirect;
-CREATE TABLE IF NOT EXISTS redirect (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  url varchar(100) NOT NULL,
-  redirect varchar(100) NOT NULL,
-  PRIMARY KEY (id)
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `redirect`
+--
+
+DROP TABLE IF EXISTS `redirect`;
+CREATE TABLE IF NOT EXISTS `redirect` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(100) NOT NULL,
+  `redirect` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS sessions;
-CREATE TABLE IF NOT EXISTS sessions (
-  session_id varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
-  expires int(11) UNSIGNED NOT NULL,
-  data mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
-  PRIMARY KEY (session_id)
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
+  PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS user;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  email varchar(100) NOT NULL,
-  username varchar(100) DEFAULT NULL,
-  password varchar(100) NOT NULL,
-  role varchar(100) DEFAULT NULL,
-  PRIMARY KEY (id)
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) NOT NULL,
+  `role` varchar(100) DEFAULT NULL,
+  `ppfile` varchar(100) NOT NULL DEFAULT '/asset/nopp.png',
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
