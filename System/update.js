013925430205d6cd831ed837899abdfea2544a43
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-06-08 20:50:17 +0200

    Clean du code

--- System/update.js
+++ System/update.js
@@ -6,75 +6,61 @@ colors.enable();
 const request = require("request");
 const AdmZip = require("adm-zip");
 const fs = require("fs");
-const path = require('path')
-const knex = require('knex')
-const knex_conf = require('../knexfile')
-const migrate_knex = knex(knex_conf.production)
+const path = require("path");
+const knex = require("knex");
+const knex_conf = require("../knexfile");
+const migrate_knex = knex(knex_conf.production);
 
-async function  migrate_db () {
-
+async function migrate_db() {
   console.log(colors.green(" Start migrating Database..."));
 
-    try {
-      await migrate_knex.migrate.latest()
-      console.log(colors.green(" Database migrated successfully!"));
-    } catch(error) {
-
-      console.error(colors.red("Error during database migration"))
-    } finally {
-      await migrate_knex.destroy()
-    }
-
-
-  
+  try {
+    await migrate_knex.migrate.latest({
+      directory: "../migrations",
+    });
+    console.log(colors.green(" Database migrated successfully!"));
+  } catch (error) {
+    console.error(colors.red("Error during database migration" + error));
+  } finally {
+    await migrate_knex.destroy();
+  }
 }
 
 function update_files() {
-  
-
   return new Promise((resolve, reject) => {
-
-    const url = 'https://update.isis-cms.thebirdproduction.fr/update/latest/' + config.update_key;
+    const url =
+      "https://update.isis-cms.thebirdproduction.fr/update/latest/" +
+      config.update_key;
     request.get(url, { encoding: null }, (error, response, body) => {
       if (error) {
-        console.error('Erreur lors du téléchargement du fichier de mise à jour :', error);
-        reject(error)
+        console.error(
+          "Erreur lors du téléchargement du fichier de mise à jour :",
+          error
+        );
+        reject(error);
         return;
       }
-  
+
       // Extraire le contenu du fichier ZIP dans le répertoire de l'application
       const zip = new AdmZip(body);
       zip.extractAllTo(env.dirname, true);
-      resolve()
-
-    })
-  
-
-    
-
-
+      resolve();
+    });
   });
 }
 
-function  restart_app() {
+function restart_app() {
   return new Promise((resolve, reject) => {
-
     exec("npm install", (error, stdout, stderr) => {
       if (error) {
-        console.error(
-          "Erreur lors de l'installation des dépendances",
-          error
-        );
+        console.error("Erreur lors de l'installation des dépendances", error);
       }
       reject(error);
     });
 
     exec("pm2 restart " + config["pm2 name app"], (error, stdout, stderr) => {
       if (error) {
-        console.error(
-          "Erreur lors du redémarrage de l'application :",
-          error
-        );
+        console.error("Erreur lors du redémarrage de l'application :", error);
 
         reject(error);
         return;
@@ -83,24 +69,17 @@ function  restart_app() {
       console.log("Application redémarrée avec succès");
       resolve();
     });
-
-
-  })
+  });
 }
 
-
- async function update()  {
+async function update() {
   try {
-    
-    await update_files()
-    await migrate_db()
-    await restart_app()
+    await update_files();
+    await migrate_db();
+    await restart_app();
   } catch (error) {
-    console.error('New error during update', error)
+    console.error("New error during update", error);
   }
-
 }
 
-exports.update = update
-
-
+exports.update = update;
