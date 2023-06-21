28fcbca86aecc4c6e53d26120197404f0180381e
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-06-21 16:52:53 +0200

    Try to resolve bug
    Cannot find module '../System/getdata' from 'Controllers/AdminControllers.js'
    
        Require stack:
          Controllers/AdminControllers.js
          Routes/AdminRoutes.js
          app.js
          test/main.test.js
    
          3 | const config = require("../config.json");
          4 | const db = require("../System/db");
        > 5 | const get = require("../System/getdata");
            |             ^
          6 |
          7 | const system_data = require("../System/ControlData");
          8 | const plugins = get.plugins;
    
          at Resolver._throwModNotFoundError (node_modules/jest-resolve/build/resolver.js:427:11)
          at Object.require (Controllers/AdminControllers.js:5:13)
          at Object.require (Routes/AdminRoutes.js:3:26)
          at Object.require (app.js:43:22)
          at Object.require (test/main.test.js:4:13)
    
    A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them.
    Test Suites: 1 failed, 1 passed, 2 total
    Tests:       1 passed, 1 total
    Snapshots:   0 total
    Time:        2.046 s
    Ran all test suites.
    
    Exited with code exit status 1
    CircleCI received exit code 1

--- Controllers/AdminControllers.js
+++ Controllers/AdminControllers.js
@@ -2,7 +2,9 @@ const env = require("../var");
 const theme = require("../themes.json");
 const config = require("../config.json");
 const db = require("../System/db");
-const get = require("../System/getdata");
+
+let get = require("../System/getdata");
+
 
 const system_data = require("../System/ControlData");
 const plugins = get.plugins;
