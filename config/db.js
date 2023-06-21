503d648c1267296ddce98b35ad92d7c78409e0b5
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-06-21 14:21:50 +0200

    Update config for circleci test

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 exports.db_host= "localhost"
-exports.db_username = "root"
-exports.db_password = ""
-exports.db_name = "isis"
+exports.db_username = process.env.DB_USER || "root"
+exports.db_password =  process.env.DB_PASSWORD || "" 
+exports.db_name = process.env.DB || "isis"
 
 exports.option = option = {
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'isis'
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || "" ,
+    database: process.env.DB || 'isis'
 }
