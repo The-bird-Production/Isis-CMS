d0e1606e9499232fb57ce9b0b46d3adea2cc26f8
Faynix <59486270+Faynix-code@users.noreply.github.com>
2023-11-18 16:48:21 +0100

    Config simplification correction

--- config/db.js
+++ config/db.js
@@ -1,11 +1,19 @@
+
+//Change this
 exports.db_host= "localhost"
 exports.db_username = process.env.DB_USER || "root"
 exports.db_password =  process.env.DB_PASSWORD || "" 
 exports.db_name = process.env.DB || "isis"
 
+
+
+
+
+
+//Don't change this 
 exports.option = option = {
-    host: 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || "" ,
-    database: process.env.DB || 'isis'
+    host: this.db_host,
+    user: this.db_username,
+    password: this.db_password,
+    database: this.db_name
 }
