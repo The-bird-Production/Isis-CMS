99f3a0e8b841cb75491ef12f8d2a851dcf754126
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-01-05 18:54:18 +0100

    Add seo options
    Adding support for robot.txt and sitemap.xml

--- Routes/main_routes.js
+++ Routes/main_routes.js
@@ -6,19 +6,16 @@ const PageControllers = require("../Controllers/PageControllers");
 const fs = require("fs");
 
 const MainControllers = require("../Controllers/MainControllers");
-const EventControllers = require('../Controllers/EventControllers')
+const EventControllers = require("../Controllers/EventControllers");
 
 const themes = require(`../Themes/${config.theme}/theme.js`);
 
 router.get("/error", EventControllers.error);
 router.get("/success", EventControllers.success);
 
-
-
 router.get("/:page([a-z]+)", PageControllers.page);
 
-
-//Asset  
+//Asset
 router.use((req, res, next) => {
   if (req.url.startsWith("/asset")) {
     const filePath = env.dirname + `/Themes/${config.theme}/` + req.url;
@@ -34,19 +31,18 @@ router.use((req, res, next) => {
   }
 });
 
-//Testing 
-router.get('/test', (req,res) => {
-  res.status(200)
-})
+//Testing
+router.get("/test", (req, res) => {
+  res.status(200);
+});
+
+router.get("/robot.txt", PageControllers.robotDotTxt);
+router.get("/sitemap.xml", PageControllers.sitemapDotXml);
 
 //Redirect
-router.get('/r/:url', MainControllers.redirect)
-
-
+router.get("/r/:url", MainControllers.redirect);
 
 //Main Page
 router.get("/", PageControllers.index);
-
-
 
 module.exports = router;
