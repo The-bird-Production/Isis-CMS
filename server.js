39cdf101bad7e1163521426e38e025ed2469e444
Faynix <59486270+Faynix-code@users.noreply.github.com>
2024-02-19 15:43:18 +0100

    Add security package

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const config = require('./config.json')
 const colors = require('colors/safe');
 require('dotenv').config()
 
-const cors = require('cors'); 
 
-app.use(cors()); 
+const cors = require('cors')
+const helmet = require('helmet')
+const compression = require('compression')
+app.use(cors({
+  origin: '*',
+  methods: [ "GET"]
+}))
+app.use(helmet())
+app.use(compression())
 
 
 app.listen(config.app_port, () => {
